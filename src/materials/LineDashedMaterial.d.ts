0362b18798f95654ba8f72aa6183179f4b468d97
fabianmuff <34745303+fabianmuff@users.noreply.github.com>
2020-10-23 11:15:11 +0200

    upload drag example

---
+++ src/materials/LineDashedMaterial.d.ts
@@ -0,0 +1,21 @@
+import { Color } from './../math/Color';
+import { LineBasicMaterial, LineBasicMaterialParameters } from './LineBasicMaterial';
+
+export interface LineDashedMaterialParameters extends LineBasicMaterialParameters {
+	scale?: number;
+	dashSize?: number;
+	gapSize?: number;
+}
+
+export class LineDashedMaterial extends LineBasicMaterial {
+
+	constructor( parameters?: LineDashedMaterialParameters );
+
+	scale: number;
+	dashSize: number;
+	gapSize: number;
+	readonly isLineDashedMaterial: true;
+
+	setValues( parameters: LineDashedMaterialParameters ): void;
+
+}
