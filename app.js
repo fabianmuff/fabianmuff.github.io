8394baf6c88c120d037aff1a68ed9652d93760c2
fabianmuff <34745303+fabianmuff@users.noreply.github.com>
2020-10-28 15:57:51 +0100

    libs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { GLTFLoader } from '../../libs/three/jsm/GLTFLoader.js';
-import { RGBELoader } from '../../libs/three/jsm/RGBELoader.js';
-import { ARButton } from '../../libs/ARButton.js';
-import { LoadingBar } from '../../libs/LoadingBar.js';
-import { Player } from '../../libs/Player.js';
+import * as THREE from '../libs/three/three.module.js';
+import { GLTFLoader } from '../libs/three/jsm/GLTFLoader.js';
+import { RGBELoader } from '../libs/three/jsm/RGBELoader.js';
+import { ARButton } from '../libs/ARButton.js';
+import { LoadingBar } from '../libs/LoadingBar.js';
+import { Player } from '../libs/Player.js';
 
 class App{
 	constructor(){
